d4b48d1a9d077a9ee4c14cc164ee5f89dd8cf4ee
GoldFar <96344369+GoldFar228@users.noreply.github.com>
2024-01-17 21:58:29 +0500

    started new project

--- First_App/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-class VacanciesAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name')
-    search_fields = ('title', 'name')
-
-admin.site.register(Vacancies, VacanciesAdmin)
-
---
+++ main/views.py
@@ -0,0 +1,56 @@
+from django.shortcuts import render
+from .models import Home, Demand, Skills, Geography
+
+
+# Create your views here.
+
+def home(request):
+    homepage = Home.objects.all()
+    return render(
+        request,
+        'home.html',
+        context={
+            'homepage': homepage,
+        }
+    )
+
+
+def info(request):
+
+    infopage = Demand.objects.all()
+    return render(
+        request,
+        'info.html',
+        context={
+            'infopage': infopage,
+        }
+    )
+
+
+def geography(request):
+    geographypage = Geography.objects.all()
+    return render(
+        request,
+        'geography.html',
+        context={
+            'geographypage': geographypage,
+        }
+    )
+
+
+def vacancies(request):
+    return render(request, 'vacanvies_template.html',
+                  context={'vacancies': get_vacancies(), })
+
+
+def skills(request):
+    skillspage = Skills.objects.all()
+    return render(
+        request,
+        'skills.html',
+        context={
+            'skillspage': skillspage,
+        }
+    )
+
+
